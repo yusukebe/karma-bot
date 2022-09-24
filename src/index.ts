db712dd7e78d595f2058c2ecd11581196437e1f4
Yusuke Wada <yusuke@kamawada.com>
2022-09-25 07:30:53 +0900

    bump up Hono and refactor

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,51 @@
-import { Hono } from 'hono'
-import { bodyParse } from 'hono/body-parse'
-
-declare let KV_KARMA: KVNamespace
-declare const SLACK_TOKEN: string
-declare const RESPONSE_TYPE: string
+import { Context, Hono } from 'hono'
+import { validator } from 'hono/validator'
 
 const PREFIX = 'v1:karma:'
 
-const app = new Hono()
-
-app.post('*', bodyParse())
-
-app.post('*', async (c) => {
-  const { token, text } = c.req.parsedBody
-
-  if (!(token == SLACK_TOKEN)) return responseUsage()
-  if (!text) return responseUsage()
-
-  const result = parse(text)
-  if (!result) return responseUsage()
-
-  const message = await karma(result.name, result.operation)
-
-  const data = {
-    text: message,
-    response_type: RESPONSE_TYPE,
-  }
-  return c.json(data)
-})
-
-const responseUsage = () => {
-  return new Response(JSON.stringify({ text: 'Usage: {name}++' }), {
-    headers: { 'content-type': 'application/json' },
-    status: 200,
-  })
+type Bindings = {
+  KV_KARMA: KVNamespace
+  SLACK_TOKEN: string
+  RESPONSE_TYPE: string
 }
 
-const parse = (text: string) => {
-  text = text.trim()
-  const regex = /^([0-9a-zA-Z]+)(\+\+|\-\-)/
-  const result = text.match(regex)
-  if (!result) return null
-  const name = result[1]
-  const operation = result[2]
-  return { name: name, operation: operation }
-}
+const app = new Hono<{ Bindings: Bindings }>()
 
-const karma = async (name: string, operation: string) => {
+app.post(
+  '*',
+  validator(
+    (v, c) => ({
+      token: v.body('token').isEqual(c.env.SLACK_TOKEN),
+      text: v.body('text').isRequired(),
+    }),
+    {
+      done: (result, c) => {
+        if (result.hasError) {
+          console.log(result.messages)
+          return responseUsage(c)
+        }
+      },
+    },
+  ),
+  async (c) => {
+    const res = c.req.valid()
+
+    const result = parse(res.text)
+    if (!result) return responseUsage(c)
+
+    const message = await karma(c.env.KV_KARMA, result.name, result.operation)
+
+    const data = {
+      text: message,
+      response_type: c.env.RESPONSE_TYPE,
+    }
+    return c.json(data)
+  },
+)
+
+const karma = async (kv: KVNamespace, name: string, operation: string) => {
   const key = PREFIX + name
-  const value = await KV_KARMA.get(key)
+  const value = await kv.get(key)
 
   let karma = value != null ? parseInt(value) : 0
 
@@ -57,9 +54,22 @@ const karma = async (name: string, operation: string) => {
   } else {
     karma = karma - 1
   }
-  await KV_KARMA.put(key, `${karma}`)
+  await kv.put(key, `${karma}`)
 
   return `${name} : ${karma}`
 }
 
-app.fire()
+const responseUsage = (c: Context) => {
+  return c.json({ text: 'Usage: {name}++' })
+}
+
+const parse = (text: string) => {
+  const regex = /^([0-9a-zA-Z]+)(\+\+|\-\-)/
+  const result = text.trim().match(regex)
+  if (!result) return null
+  const name = result[1]
+  const operation = result[2]
+  return { name, operation }
+}
+
+export default app
